60ddeed8b946d1f3d681d853026cf25e29f8cf59
Kai Jie Lee <94023052+lkaijie@users.noreply.github.com>
2023-04-15 18:37:58 -0600

    Config template for API keys

---
+++ config(template).py
@@ -0,0 +1,13 @@
+# (twitter)
+API_Key = ""
+API_Key_Secret = ""
+Bearer = ""
+Access_Token = ""
+Access_Token_Secret = ""
+#  (discord)
+Client_ID = ""
+Client_Secret = ""
+client_discord_token = ""
+# (firebase)
+firebase_config = {
+}
